831503c182c4673b0205f6a563025c4bd7423682
Ahmedgomaaabdelrahman <a.gomaaabdelrahman@gmail.com>
2017-03-26 08:30:35 +0200

    fixed design

--- src/pages/driver-license/driver-license.ts
+++ src/pages/driver-license/driver-license.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {DriverCarPage} from "../driver-car/driver-car";
-import { CustomToast} from "../../general-components/toast.component";
 /*
   Generated class for the DriverLicense page.
 
@@ -15,12 +14,9 @@ import { CustomToast} from "../../general-components/toast.component";
 export class DriverLicensePage {
   public user;
   public pushPage;
-  constructor(public navCtrl: NavController, public navParams: NavParams
-  ,private customToast : CustomToast) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.user = navParams.data;
     this.pushPage = DriverCarPage;
-    customToast.toast(this.user.id_front);
-    customToast.toast(this.user.id_front);
  }
 
   ionViewDidLoad() {
